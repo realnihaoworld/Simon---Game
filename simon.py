3891bde99cb86a4562bdfe0929e53dd7400b9f2c
cdb111 <159564643+cdb111@users.noreply.github.com>
2024-02-25 13:57:28 -0600

    Added led level up indicator

--- simon.py
+++ simon.py
@@ -1,53 +1,10 @@
-# Names: Danison Zhang, Colin Bass, Jacob James
-
-import pineworkslabs.RPi as GPIO
-from time import sleep
-from random import choice
-import pygame
-from pygame.mixer import Sound
-import os
-
-pygame.init()
-
-GPIO.setmode(GPIO.LE_POTATO_LOOKUP)
-
-class Button:
-
-    def __init__(self, switch:int, led:int, sound:str, color:str):
-        self.switch = switch
-        self.led = led
-        self.sound: Sound = Sound(sound)
-        self.color = color
-        self.setupGPIO()
-        
-    def setupGPIO(self):
-        GPIO.setup(self.switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.led, GPIO.OUT)
-        
-    def turn_light_on(self):
-        GPIO.output(self.led, True)
-                    
-    def turn_light_off(self):
-        GPIO.output(self.led, False)
-        
-    def is_pressed(self):
-        # returns true or false depending on the switch
-        return GPIO.input(self.switch)
-        
-    def respond(self):
-        self.turn_light_on()
-        self.sound.play()
-        sleep(1)
-        self.turn_light_off()
-        sleep(0.25)
-    
-    def __str__(self):
+ def __str__(self):
         return self.color
-        
+
 class Simon:
-    
+
     WELCOME_MESSAGE = "Welcome to Simon! Press ctrl+c to quit at anytime"
-    
+
     # Paths should use os.path.join(), so it works on all machines
     # do not use C:\\
     # Do not use the root directory of the VS code project
@@ -57,15 +14,15 @@ class Simon:
         Button(switch=12, led=19, sound=os.path.join("sounds", "three.wav"), color="yellow"),
         Button(switch=26, led=21, sound=os.path.join("sounds", "four.wav"), color="green")
     ]
-    
+
     def __init__(self, debug=True):
         self.debug = debug
         self.sequence: list[Button] = [] # python 3.10+ only, for the type hinting
-        
+
     def debug_out(self, *args):
         if self.debug:
             print(*args)
-            
+
     def blink_all_buttons(self):
         # leds = []
         # for button in Simon.BUTTONS:
@@ -74,26 +31,26 @@ class Simon:
         # sleep(0.5)
         # GPIO.output(leds, False)
         # sleep(0.5)
-    
+
         for button in Simon.BUTTONS:
             button.turn_light_on()
             sleep(0.5)
             button.turn_light_off()
             sleep(0.5)
-            
+
     def add_to_sequence(self):
         random_button = choice(Simon.BUTTONS)
         self.sequence.append(random_button)
-    
+
     def lose(self):
         for _ in range(4):
             self.blink_all_buttons()
         GPIO.cleanup()
-    
+
     def playback(self):
         for button in self.sequence:
             button.respond()
-    
+
     def wait_for_press(self):
         while True:
             for button in Simon.BUTTONS:
@@ -101,19 +58,30 @@ class Simon:
                     self.debug_out(button.color)
                     button.respond()
                     return button
-    
+
     def check_input(self, pressed_button, correct_button):
         if pressed_button.switch != correct_button.switch:
             self.lose()
-            
-    
+
+    def light_show(self):
+        """A function to ripple lights"""
+        for button in Simon.BUTTONS:
+            button.turn_light_on()
+            sleep(0.5)
+            button.turn_light_off()
+
+        for button in reversed(Simon.BUTTONS):
+            button.turn_light_on()
+            sleep(0.5)
+            button.turn_light_off()
+
     def run(self):
         print(Simon.WELCOME_MESSAGE)
-        
+
         # game will start with 2 items in the sequence
         self.add_to_sequence()
         self.add_to_sequence()
-        
+
         # use control+c to kill the game
         try:
             while True:
@@ -123,9 +91,16 @@ class Simon:
                 for button in self.sequence:
                     pressed_button = self.wait_for_press()
                     self.check_input(pressed_button, button)
-                    
+
+                if len(self.sequence) in [15, 20, 25, 30, 35, 40, 45, 50]:   # Checks the score for when to run light_show.
+                    self.light_show()
+
+                # USE THIS WHEN TESTING
+                #if len(self.sequence) in [1, 2, 3, 4, 5]: # Debug
+                    #self.light_show()
+
         except KeyboardInterrupt:
             GPIO.cleanup()
-            
+
 simon = Simon()
 simon.run()
